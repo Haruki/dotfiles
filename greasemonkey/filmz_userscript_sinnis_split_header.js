8d29ff8c4dcfbe8f1dc29652e0723cabc672964d
Harald Borsutzky <borsutzky@hansaluftbild.de>
2018-08-31 16:56:23 +0200

    developing html template for imdb web api data

---
+++ greasemonkey/filmz_userscript_sinnis_split_header.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name		Greasemonkey Bestfilmz (dropwizard server) v1
+// @namespace 	http://www.oreilly.com/catalog/greasemonkeyhacks/
+// @description	BF Greasemonkey Client v1
+// @include        http://www.imdb.com/title/tt*/*
+// @include        https://www.imdb.com/title/tt*/*
+// @include        http://imdb.com/title/tt*
+// @include        https://imdb.com/title/tt*
+// @exclude         http://*.media-imdb.com/*
+// @exclude			http://www.imdb.com/rt/*
+// @exclude 		http://www.imdb.com/images/*
+// @exclude			http://www.imdb.com/widget/*
+// @exclude			http://www.imdb.com/title/tt*/_ajax/*
+// @exclude 		http://www.imdb.com/title/tt*//event/*
+// @require         file:///C:/Users/borsutzha/dotfiles/greasemonkey/filmz_userscript_sinnis_split.js
+// @grant			GM_xmlhttpRequest
+// @grant           GM_setValue
+// @grant           GM_getValue
+// @connect         pimpelkram.com
+// @connect         omdbapi.com
+// @connect         themoviedb.org
+// @resource        customCSS file:///C:/Users/borsutzha/dotfiles/greasemonkey/imdb.css
+// @grant           GM_addStyle
+// @grant           GM_getResourceText
+// ==/UserScript==
