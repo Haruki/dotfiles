858ef05b3919d0a211c682f19978c3211f1ccc17
pimpelkram <hb@hb.gl>
2019-06-07 13:09:22 +0000

    basic install 2 update

--- arch/basic-install_2.sh
+++ arch/basic-install_2.sh
@@ -14,3 +14,19 @@ echo LANG=de_DE.UTF-8 > /etc/locale.conf
 
 
 cat /etc/locale.gen | sed 's/#de_DE/de_DE/' | sed 's/#en_US.ISO/en_US.ISO/' > /etc/locale.gen 
+
+locale-gen
+
+echo KEYMAP=de-latin1-nodeadkeys > /etc/vconsole.conf
+echo FONT=lat9w-16 >> /etc/vconsole.conf
+
+# timezone
+
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+echo "127.0.0.1	 	localhost.localdomain		localhost" >> /etc/hosts
+echo "::1		localhost.localdomain		localhost" >> /etc/hosts
+
+
+
+
