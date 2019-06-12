942343440ffca5cf4ebb28981808b534d3a67883
Pimpelkram <borsutzky@hansaluftbild.de>
2019-06-12 12:31:27 +0200

    Update virtualbox-efi-install.sh
    bugfix: changed arch to grub

--- arch/virtualbox-efi-install.sh
+++ arch/virtualbox-efi-install.sh
@@ -6,6 +6,6 @@
 mkdir /boot/EFI/boot
 cp /boot/EFI/grub/grubx64.efi /boot/EFI/BOOT/BOOTX86.EFI
 
-echo 'bcfg boot add 1 fs0:/EFI/arch/grubx64.efi "Manually Added"' > /boot/startup.nsh
+echo 'bcfg boot add 1 fs0:/EFI/grup/grubx64.efi "Manually Added"' > /boot/startup.nsh
 echo 'exit' >> /boot/startup.nsh
 
