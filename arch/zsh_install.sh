6a76e39c200ddfe1b8ea52a297bf0a08d5794612
pimpelkram <hb@hb.gl>
2019-10-30 01:17:51 +0100

    bugfix

--- arch/zsh_install.sh
+++ arch/zsh_install.sh
@@ -7,6 +7,8 @@
 sudo pacman -S zsh
 cd ~
 
+
+
 #zsh extensions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
