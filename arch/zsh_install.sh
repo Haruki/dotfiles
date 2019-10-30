6884d7403b2f7fa29d8605720875fd569701654c
pimpelkram <hb@hb.gl>
2019-10-30 01:16:42 +0100

    started zsh install with fonts and extensions

---
+++ arch/zsh_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+#prerequisits:
+#standard user must be active
+
+sudo pacman -S zsh
+cd ~
+
+#zsh extensions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
