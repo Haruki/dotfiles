f349c1ad898c893154111e491dacd52552232782
Harald Borsutzky <borsutzky@hansaluftbild.de>
2019-10-31 11:55:01 +0100

    dwm statusbar monitor status nerdfont icons

--- dwm_statusbar/displays.sh
+++ dwm_statusbar/displays.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 
+LAPTOP_ON="$(echo -e '\uf821')"
+LAPTOP_OFF="$(echo -e '\ufbe5')"
+EXT_ON="$(echo -e '\uf26c')"
+EXT_OFF="$(echo -e '\ufd39')"
+
+echo $LAPTOP_ON $LAPTOP_OFF $EXT_ON $EXT_OFF
+
 RESULT=""
 while IFS= read -r displayLine
 do
